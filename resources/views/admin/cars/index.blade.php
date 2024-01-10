d0b4ebe2a8098044ba4975d41c0d3ee2e05204ca
Jesse-js <jesse-jjs@hotmail.com>
2024-01-09 22:50:50 -0300

    feat: cars list structure

--- resources/views/admin/cars/index.blade.php
+++ resources/views/admin/cars/index.blade.php
@@ -4,5 +4,19 @@
             {{ __('Cars') }}
         </h2>
     </x-slot>
-    
+    <button type="button" class=" mt-2 ml-5 btn btn-success">New</button>
+    <div class="card mt-3 ml-5" style="width: 18rem;">
+        <img src="{{ asset('img/fiat.jpeg')}}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+            <span class="d-flex justify-content-between">
+                <a href="#" class="btn btn-primary">View</a>
+                <a href="#" class="btn btn-warning">Edit</a>
+                <a href="#" class="btn btn-danger">Delete</a>
+            </span>
+        </div>
+    </div>
+
 </x-app-layout>
